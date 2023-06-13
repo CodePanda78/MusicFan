6d9729e281eaa9a13be331d518dfbc7a7ba82f63
Angel@TAH <104644369+CodePanda78@users.noreply.github.com>
2023-06-13 13:13:49 -0500

    Add files via upload

--- MusicFan/Avance Player/Reproductor.php
+++ MusicFan/Avance Player/Reproductor.php
@@ -1,5 +1,10 @@
 <html>
-  <link rel="stylesheet" href="Reproductor.css">
+<head>
+    <meta charset="UTF-8">
+    <title>Mi música</title>
+    <link rel="stylesheet" href="Reproductor.css">
+</head>
+<body>
 <?php
 session_start();
 
@@ -29,93 +34,103 @@ if ($_SESSION["token"] == "SI") {
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        // Mostrar las canciones en tabla (modificar con CSS)
-        echo "<table>
-                <tr>
-                    <th>ID</th>
-                    <th>Autor</th>
-                    <th>Cancion</th>
-                    <th></th>
-                </tr>";
+        // Output data of each row
+        $songs = [];
         while ($row = $result->fetch_assoc()) {
-            echo "<tr>
-                    <td>".$row['ID']."</td>
-                    <td>".$row['Autor']."</td>
-                    <td>".$row['Cancion']."</td>
-                    <td>
-                        <form action='BorrarFavoritos.php' method='post'>
-                            <input type='hidden' name='cancion_id' value='".$row['ID']."'>
-                            <input type='submit' value='Borrar' class='my-btn'>
-                        </form>
-                    </td>
-                </tr>";
+            $songs[] = [
+                'id' => $row['ID'],
+                'author' => $row['Autor'],
+                'name' => $row['Cancion'],
+                'link' => "../$row[Link]"
+            ];
         }
-        echo "</table>";
+
+        $currentSongIndex = 0;
+        $currentSong = $songs[$currentSongIndex];
+        $totalSongs = count($songs);
+
+        echo "
+        <div class='container'>
+            <div class='player'>
+                <div class='player__controls'>
+                    <div class='player__btn player__btn--small' id='speed-btn'>1x</div>
+                    <h5 class='player__title'>MUSIC FAN</h5>
+                    <a class='player__btn player__btn--small' id='icon-menu' href='{$currentSong['link']}' download>
+                        <p class='fas fa-bars'>&#10515;</p>
+                    </a>
+                </div>
+                <div class='player__album'>
+                    <img src='reproductor-placeholder.png' alt='Album cover' class='player__img' loading='lazy' />
+                </div>
+                <h2 class='player__artist'>{$currentSong['name']}</h2>
+                <h3 class='player__song'>{$currentSong['author']}</h3>
+                <div class='timeline'>
+                    <input type='range' id='progress-bar' min='0' value='0'>
+                    <div id='timer'>0:00 / 0:00</div>
+                </div>
+                <audio class='player__audio' controls id='audio-player'>
+                    <source src='{$currentSong['link']}' type='audio/mpeg' />
+                </audio>
+                <div class='player__controls'>
+                    <div id='backward-btn' class='player__btn player__btn--medium'>&#9668;&#9668;</div>
+                    <div id='play-pause-btn' class='player__btn player__btn--medium blue play'>&#9658;</div>
+                    <div id='forward-btn' class='player__btn player__btn--medium'>&#9658;&#9658;</div>
+                </div>
+            </div>
+        </div>
+        <script src='Reproductor.js'></script>
+        <script>
+            var songs = " . json_encode($songs) . ";
+            var currentSongIndex = $currentSongIndex;
+            var audioPlayer = document.getElementById('audio-player');
+
+            function playSong(index) {
+                var song = songs[index];
+                audioPlayer.src = song.link;
+                audioPlayer.load();
+                audioPlayer.play();
+                document.querySelector('.player__artist').textContent = song.name;
+                document.querySelector('.player__song').textContent = song.author;
+                document.querySelector('#icon-menu').href = song.link;
+            }
+
+            document.querySelector('#play-pause-btn').addEventListener('click', function() {
+                if (audioPlayer.paused) {
+                    audioPlayer.play();
+                    this.classList.remove('play');
+                    this.classList.add('pause');
+                } else {
+                    audioPlayer.pause();
+                    this.classList.remove('pause');
+                    this.classList.add('play');
+                }
+            });
+
+            document.querySelector('#forward-btn').addEventListener('click', function() {
+                currentSongIndex++;
+                if (currentSongIndex >= songs.length) {
+                    currentSongIndex = 0;
+                }
+                playSong(currentSongIndex);
+            });
+
+            document.querySelector('#backward-btn').addEventListener('click', function() {
+                currentSongIndex--;
+                if (currentSongIndex < 0) {
+                    currentSongIndex = songs.length - 1;
+                }
+                playSong(currentSongIndex);
+            });
+        </script>
+        ";
     } else {
         echo "No tienes canciones favoritas en tu biblioteca";
     }
 
     $conn->close();
-}
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "musicfan";
-$idSong = 1;
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT ID, Portada, Autor, Cancion, Link FROM musica WHERE ID = $idSong ";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // Output data of each row
-    while($row = $result->fetch_assoc()) {
-        $portadaSong = "../$row[Portada]";
-        $nameSong = $row['Cancion'];
-        $autorSong = $row['Autor'];
-        $linkSong = "../$row[Link]";//
-
-        echo "
-            <div class='container'>
-                <div class='player'>
-                    <div class='player__controls'>
-                        <div class='player__btn player__btn--small' id='speed-btn'>1x</div>
-                        <h5 class='player__title'>MUSIC FAN</h5>
-                        <a class='player__btn player__btn--small' id='icon-menu' href='$linkSong' download>
-                            <p class='fas fa-bars'>&#10515;</p>
-                        </a>
-                    </div>
-                    <div class='player__album'>
-                        <img src='$portadaSong' alt='Album cover' class='player__img' loading='lazy' />
-                    </div>
-                    <h2 class='player__artist'>$nameSong</h2>
-                    <h3 class='player__song'>$autorSong</h3>
-                    <div class='timeline'>
-                        <input type='range' id='progress-bar' min='0' value='0'>
-                        <div id='timer'>0:00 / 0:00</div>
-                    </div>
-                    <audio class='player__audio' controls id='audio-player'>
-                        <source src='$linkSong' type='audio/mpeg' />
-                    </audio>
-                    <div class='player__controls'>
-                        <div id='backward-btn' class='player__btn player__btn--medium'>&#9668;&#9668;</div>
-                        <div id='play-pause-btn' class='player__btn player__btn--medium blue play'>&#9658;</div>
-                        <div id='forward-btn' class='player__btn player__btn--medium'>&#9658;&#9658;</div>
-                    </div>
-                </div>
-            </div>
-            <script src='Reproductor.js'></script>
-        ";
-    } 
 } else {
-    echo "Ups ha ocurrido un error :(";
+    echo "Debes iniciar sesión para acceder a esta página";
 }
-$conn->close();
 ?>
+</body>
 </html>
