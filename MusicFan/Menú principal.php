27b4d188ac9632f33c0d80cede745764911dc7a2
Angel@TAH <104644369+CodePanda78@users.noreply.github.com>
2023-06-12 23:33:47 -0500

    Add files via upload

--- MusicFan/Menú principal.php
+++ MusicFan/Menú principal.php
@@ -14,12 +14,11 @@ if ($_SESSION["token"] == "SI") {
     </head>
 <header> <m>M</m>usic <f>F</f>an
     <div class='dropdown-container'>
-       <img src='Portada\User.png'> <button class='dropdown-btn'>User</button> </img>
+       <img src = 'Portada\User.png'> <button class='dropdown-btn'>User</button> </img>
         <div class='dropdown-content'>
             <a href='Demosession.php'>Yo</a> <br> <br>
-            <a href='IniciarSesion.html'>Iniciar Sesión</a> <br> <br>
             <a href='Biblioteca.php'>Biblioteca</a> <br> <br>
-            <a href='CerrarSession.php'>Cerrar sesión</a>
+            <a href='CerrarSession.php'>Cerrar sesion</a>
         </div>
     </div>
 </header>
@@ -30,25 +29,62 @@ if ($_SESSION["token"] == "SI") {
     <button class='my-btn2' onclick=\"location.href='Descargar.php'\">Descargas</button>
      <!--Botón 2 desplegable-->
     <div class='dropdown-container2'>
-        <button class='my-btn2'>Género</button>
+        <button class='my-btn2'>Genero</button>
         <div class='dropdown-content2'>
-            <a href='rock.html'>Rock</a> <br>
-            <a href='Indie.html'>Indie</a> <br>
-            <a href='pop.html'>Underground</a> <br>
-            <a href='pop.html'>Latino</a> <br>
-        </div>
-    </div>
-    </div>
+  <a href='rock.html'>Rock</a> <br>
+  <a href='Indie.html'>Indie</a> <br>
+  <a href='pop.html'>Undergorund</a> <br>
+  <a href='pop.html'>Latino</a> <br>
+
+</div>
+</div>
+</div>
 </nav>
 <!--Content-->
 <content>
 <br>
 
-<div class='imagenes'>
-    <img src='Portada\SpaceJunk.jpg' class='imagen-modificada'> <br> <br>
-    <audio controls class='audio'>
-    <source src='music\Space Junk - Wang Chung.mp3' type='audio/mpeg'>
-  </audio>
+<h2> Lo más Nuevo!</h2>
+
+<div class='container'>
+    <div class='imagenes'>
+      <img src='Portada\SpaceJunk.jpg' class='imagen-modificada'> <br> <br>
+      <button onclick='playAudio()'>Play</button>
+      <button onclick='pauseAudio()'>Pause</button>
+      <audio id='myAudio'>
+        <source src='music/Space Junk - Wang Chung.mp3' type='audio/mpeg'>
+      </audio>
+    </div>
+  
+    <div class='imagenesD'>
+      <img src='Portada\DizzyPortada.jpg' class='imagen-modificadaD'> <br> <br>
+      <button onclick='playAudioD()'>Play</button>
+      <button onclick='pauseAudioD()''>Pause</button>
+      <audio id='myAudioD'>
+        <source src='music\Dizzy - Joakim Karud (320).mp3' type='audio/mpeg'>
+      </audio>
+    </div>
+
+    <div class='imagenesG'>
+        <img src='Portada\GoShoppingPortada.jpeg' class='imagen-modificadaG'> <br> <br>
+        <button onclick='playAudioG()'>Play</button>
+        <button onclick='pauseAudioG()'>Pause</button>
+        <audio id='myAudioG'>
+          <source src='music\Go Shopping - Bran Van 3000 (320).mp3' type='audio/mpeg'>
+        </audio>
+      </div>
+  </div>
+  
+    
+     <h2>Lo mejor de Nirvana!</h2>
+     <div class='imagenes2'>
+        <img src='Portada\CAYAPortada.jpeg' class='imagen-modificada2'> <br> <br>
+        <button onclick='playAudio2()'>Play</button>
+        <button onclick='pauseAudio2()'>Pause</button>
+        <audio id='myAudio2'>
+          <source src='music\Come As You Are - Nirvana.mp3' type='audio/mpeg'>
+        </audio>
+         <script src='Menú.js'></script>
 
 </div>
 </content>
@@ -68,7 +104,6 @@ if ($_SESSION["token"] == "SI") {
             <a href='Demosession.php'>Yo</a> <br> <br>
             <a href='IniciarSesion.html'>Iniciar Sesión</a> <br> <br>
             <a href='Biblioteca.php'>Biblioteca</a> <br> <br>
-            <a href='CerrarSession.php'>Cerrar sesión</a>
         </div>
     </div>
 </header>
---
+++ MusicFan/Menú.js
@@ -0,0 +1,73 @@
+//LO MAS NUEVO
+var audio = document.getElementById('myAudio');
+
+function playAudio() {
+  audio.play();
+}
+
+function pauseAudio() {
+  audio.pause();
+}
+
+var audioD = document.getElementById('myAudioD');
+
+function playAudioD() {
+  audioD.play();
+}
+
+function pauseAudioD() {
+  audioD.pause();
+}
+
+var audioG = document.getElementById('myAudioG');
+
+function playAudioG() {
+  audioG.play();
+}
+
+function pauseAudioG() {
+  audioG.pause();
+}
+var audioS = document.getElementById('myAudioS');
+
+function playAudioS() {
+  audioS.play();
+}
+
+function pauseAudioS() {
+  audioS.pause();
+}
+
+
+
+//nirvana
+
+var audio2 = document.getElementById('myAudio2');
+
+function playAudio2() {
+  audio2.play();
+}
+
+function pauseAudio2() {
+  audio2.pause();
+}
+
+var audio3 = document.getElementById('myAudio3');
+
+function playAudio3() {
+  audio3.play();
+}
+
+function pauseAudio3() {
+  audio3.pause();
+}
+
+var audio4 = document.getElementById('myAudio4');
+
+function playAudio4() {
+  audio4.play();
+}
+
+function pauseAudio4() {
+  audio4.pause();
+}
